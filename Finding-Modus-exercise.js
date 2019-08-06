571d91f9a7edbe059aba71bc71806a93fd8149c5
Panji SN <panji1595@gmail.com>
2019-08-06 18:04:34 +0700

    Finding-Modus-exercise

---
+++ Finding-Modus-exercise.js
@@ -0,0 +1,36 @@
+function cariModus(arr) {
+    var bilanganModus = 0;
+    var temp = 0;
+    var angkaSama = 0;
+    var hasil = 0;
+    for (var i = 0;i<arr.length;i++){
+        temp = 0;
+        for (var j = 0;j<arr.length;j++){
+            if (arr[i]==arr[j] && i!==j){
+                temp ++;
+                if (temp > bilanganModus){
+                    bilanganModus = temp;
+                    hasil = i;
+                } 
+            } 
+        }
+        
+    }
+    if (bilanganModus === 0){
+        return -1;
+    }
+    for (var k = 0; k<arr.length;k++){
+        angkaSama = angkaSama + arr[k];
+        if (angkaSama / arr.length == arr[k]) {
+            return -1;
+        }
+    }
+    return arr[hasil];
+  }
+  
+  // TEST CASES
+  console.log(cariModus([10, 4, 5, 2, 4])); // 4
+  console.log(cariModus([5, 10, 10, 6, 5])); // 5
+  console.log(cariModus([10, 3, 1, 2, 5])); // -1
+  console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+  console.log(cariModus([7, 7, 7, 7, 7])); // -1

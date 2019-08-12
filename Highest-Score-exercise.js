4fdb670b8e4d946045bfe171e0199436ba57ac68
Panji SN <panji1595@gmail.com>
2019-08-12 22:37:50 +0700

    Adding Highest-Score-exercise

---
+++ Highest-Score-exercise.js
@@ -0,0 +1,87 @@
+function highestScore (students) {
+    //Inisialisasi Variabel
+    var objTotal = {};
+    var nilai = 0;
+    var nama = "";
+    var kelas = "";
+    //return students.length;
+    //Proses
+    for (var i = 0; i < students.length;i++){
+        kelas = students[i].class;
+        nilai = students[i].score;
+        nama = students[i].name;
+        if (!objTotal[kelas] || nilai > objTotal[kelas].score){
+            objTotal[kelas] = {name: nama, score:nilai};
+        }
+        //objTotal.kelas = obj;
+    }
+    //Output
+    return objTotal;
+  }
+  
+  // TEST CASE
+  console.log(highestScore([
+    {
+      name: 'Dimitri',
+      score: 90,
+      class: 'foxes'
+    },
+    {
+      name: 'Alexei',
+      score: 85,
+      class: 'wolves'
+    },
+    {
+      name: 'Sergei',
+      score: 74,
+      class: 'foxes'
+    },
+    {
+      name: 'Anastasia',
+      score: 78,
+      class: 'wolves'
+    }
+  ]));
+  
+  // {
+  //   foxes: { name: 'Dimitri', score: 90 },
+  //   wolves: { name: 'Alexei', score: 85 }
+  // }
+  
+  
+  console.log(highestScore([
+    {
+      name: 'Alexander',
+      score: 100,
+      class: 'foxes'
+    },
+    {
+      name: 'Alisa',
+      score: 76,
+      class: 'wolves'
+    },
+    {
+      name: 'Vladimir',
+      score: 92,
+      class: 'foxes'
+    },
+    {
+      name: 'Albert',
+      score: 71,
+      class: 'wolves'
+    },
+    {
+      name: 'Viktor',
+      score: 80,
+      class: 'tigers'
+    }
+  ]));
+  
+  // {
+  //   foxes: { name: 'Alexander', score: 100 },
+  //   wolves: { name: 'Alisa', score: 76 },
+  //   tigers: { name: 'Viktor', score: 80 }
+  // }
+  
+  
+  console.log(highestScore([])); //{}
